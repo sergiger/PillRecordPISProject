224e9e6acb2bcd25558bd125a9d9604af4409bc2
alendinez <55415649+alendinez@users.noreply.github.com>
2020-03-18 19:10:03 +0100

    ActionBar eliminada

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.afontgou17alumnes.mypillrecord
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
