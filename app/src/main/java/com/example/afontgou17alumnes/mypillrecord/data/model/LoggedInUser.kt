b7cc4c577f2da1f57e8f550f2ceff8c85642d355
Sergi Ger <sergiger00@gmail.com>
2020-03-19 02:41:19 +0100

    Login Activity started

---
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.afontgou17alumnes.mypillrecord.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
