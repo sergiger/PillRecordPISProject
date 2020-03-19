e2b54f11e8ae46bb6645ca9c1ce80100dabbdaec
Agfont <arthurfont2000@gmail.com>
2020-03-19 16:17:30 -0300

    Layout: Calendar 3 Fragments (Week, Month, List)

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/calendar/PagerAdapter.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/calendar/PagerAdapter.kt
@@ -4,24 +4,26 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 
-class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm){
+class PagerAdapter(val myContext: Calendar_fragment, fm: FragmentManager?, internal var totalTabs: Int) : FragmentPagerAdapter(fm) {
+
     override fun getItem(position: Int): Fragment {
         return when (position) {
-            0-> { FragmentOne() }
-            1-> { FragmentTwo() }
+            0-> { return FragmentOne() }
+            1-> { return FragmentTwo() }
             else-> { return FragmentThree() }
         }
     }
 
     override fun getCount(): Int {
-        return 3
+        return totalTabs
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
         return when (position) {
             0-> { "One"}
             1-> { "Two"}
-            else-> { return "Three"}
+            else -> return "Three"
         }
     }
+
 }
