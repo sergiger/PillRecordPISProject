6887a8af341d2835adbbfafe4dd35dde274203a3
Sergi Ger <sergiger00@gmail.com>
2020-03-19 21:47:23 +0100

    joan task's some fixed problems

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.afontgou17alumnes.mypillrecord
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.afontgou17alumnes.mypillrecord.Pill.Pill_fragment
 import com.example.afontgou17alumnes.mypillrecord.calendar.Calendar_fragment
 import com.example.afontgou17alumnes.mypillrecord.statistics.Statistics_fragment
@@ -62,5 +65,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu,menu)
         return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        if(id==R.id.options_item){
+            Toast.makeText(this,"opcions",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ajustes_activity::class.java);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/activity_Register4.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/activity_Register4.kt
@@ -1,12 +1,20 @@
 package com.example.afontgou17alumnes.mypillrecord
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity__register4.*
 
 class activity_Register4 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity__register4)
+        //Això és per a veure el flow, no és per a fer que funcionin els botons de veritat
+        btn_confirm_register.setOnClickListener {
+            // Handler code here.
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
