658d3aa4b0297f4bd28fe5125fdb9617cdd6ca52
alendinez <55415649+alendinez@users.noreply.github.com>
2020-03-19 10:54:45 +0100

    Toolbar

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.afontgou17alumnes.mypillrecord
 import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.view.Menu
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
+    }
 
-
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        return true
     }
 }
