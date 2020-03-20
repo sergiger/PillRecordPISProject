ebfcee44f48b2fc234d1cb606b4ac8df057850d4
joanorteu99 <Joanorteu99@gmail.com>
2020-03-20 21:53:28 +0100

    petits arreglos

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/settings/legalInformation/privacy.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/settings/legalInformation/privacy.kt
@@ -4,14 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toolbar
+import android.widget.*
 import com.example.afontgou17alumnes.mypillrecord.R
 import com.example.afontgou17alumnes.mypillrecord.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.ajustes_activity.*
 import kotlinx.android.synthetic.main.legal_main_activity.*
+import kotlinx.android.synthetic.main.legal_main_activity.back_legal_icon
+import kotlinx.android.synthetic.main.privacy_activity.*
+
 class privacy : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +20,9 @@ class privacy : AppCompatActivity() {
 
         back_legal_icon.setOnClickListener {
             onBackPressed()
+        }
+        print_icon.setOnClickListener {
+            Toast.makeText(this,"downloading PDF",Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/settings/legalInformation/smartPatients.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/settings/legalInformation/smartPatients.kt
@@ -2,8 +2,11 @@ package com.example.afontgou17alumnes.mypillrecord.ui.settings.legalInformation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.afontgou17alumnes.mypillrecord.R
 import kotlinx.android.synthetic.main.legal_main_activity.*
+import kotlinx.android.synthetic.main.legal_main_activity.back_legal_icon
+import kotlinx.android.synthetic.main.privacy_activity.*
 
 class smartPatients : AppCompatActivity() {
 
@@ -13,6 +16,10 @@ class smartPatients : AppCompatActivity() {
 
         back_legal_icon.setOnClickListener {
             onBackPressed()
+        }
+
+        print_icon.setOnClickListener {
+            Toast.makeText(this,"downloading PDF", Toast.LENGTH_SHORT).show()
         }
     }
 }
