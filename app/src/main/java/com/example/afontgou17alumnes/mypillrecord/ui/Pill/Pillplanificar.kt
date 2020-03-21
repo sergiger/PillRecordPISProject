b99402840035b4fa3f8ee46093748d774228b1c0
Sergi Ger <sergiger00@gmail.com>
2020-03-21 19:28:49 +0100

    Starting Mesurements

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/Pill/Pillplanificar.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/Pill/Pillplanificar.kt
@@ -1,9 +1,11 @@
 package com.example.afontgou17alumnes.mypillrecord.ui.Pill
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import com.example.afontgou17alumnes.mypillrecord.R
+import kotlinx.android.synthetic.main.activity_pillplanificar.*
 
 class Pillplanificar : AppCompatActivity() {
 
@@ -14,5 +16,10 @@ class Pillplanificar : AppCompatActivity() {
         image_view.setOnClickListener {
             onBackPressed()
         }
+        btn_medicacio.setOnClickListener{
+            val intent = Intent(this,PillMedication::class.java)
+            startActivity(intent)
+        }
+
     }
 }
