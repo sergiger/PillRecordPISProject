bb815bd66d3539d182fd4d063c095efe08a61b86
alendinez <55415649+alendinez@users.noreply.github.com>
2020-03-22 19:53:05 +0100

    Add unplanned entry date setter

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/today/AddUnplannedActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/today/AddUnplannedActivity.kt
@@ -3,9 +3,16 @@ package com.example.afontgou17alumnes.mypillrecord.ui.today
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.widget.Toast
 import com.example.afontgou17alumnes.mypillrecord.MainActivity
 import com.example.afontgou17alumnes.mypillrecord.R
 import kotlinx.android.synthetic.main.activity_add_unplanned_activity.*
+import kotlinx.android.synthetic.main.activity_add_unplanned_activity.back_arrow
+import kotlinx.android.synthetic.main.activity_add_unplanned_activity.date_button
+import kotlinx.android.synthetic.main.activity_add_unplanned_medicine.*
+import kotlinx.android.synthetic.main.specific_dates_dialoge.view.*
 
 class AddUnplannedActivity : AppCompatActivity() {
 
@@ -18,6 +25,22 @@ class AddUnplannedActivity : AppCompatActivity() {
         tick_unplanned_activity.setOnClickListener{
             val tickIntent = Intent(this, MainActivity::class.java)
             startActivity(tickIntent)
+        }
+        date_button.setOnClickListener{
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.date_dialoge, null)
+            //AlertDialogBuilder
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Set date")
+            val mAlertDialog = mBuilder.show()
+            mDialogView.OK.setOnClickListener {
+                Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+                mAlertDialog.dismiss()
+            }
+            mDialogView.cancel.setOnClickListener {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                mAlertDialog.dismiss()
+            }
         }
     }
 }
--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/today/AddUnplannedMedicine.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/today/AddUnplannedMedicine.kt
@@ -3,10 +3,14 @@ package com.example.afontgou17alumnes.mypillrecord.ui.today
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.widget.Toast
 import com.example.afontgou17alumnes.mypillrecord.MainActivity
 import com.example.afontgou17alumnes.mypillrecord.R
 import kotlinx.android.synthetic.main.activity_add_unplanned_activity.back_arrow
 import kotlinx.android.synthetic.main.activity_add_unplanned_medicine.*
+import kotlinx.android.synthetic.main.specific_dates_dialoge.view.*
 
 class AddUnplannedMedicine : AppCompatActivity() {
 
@@ -19,6 +23,22 @@ class AddUnplannedMedicine : AppCompatActivity() {
         tick_unplanned_medicine.setOnClickListener{
             val tickIntent = Intent(this, MainActivity::class.java)
             startActivity(tickIntent)
+        }
+        date_button.setOnClickListener{
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.date_dialoge, null)
+            //AlertDialogBuilder
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Set date")
+            val mAlertDialog = mBuilder.show()
+            mDialogView.OK.setOnClickListener {
+                Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+                mAlertDialog.dismiss()
+            }
+            mDialogView.cancel.setOnClickListener {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                mAlertDialog.dismiss()
+            }
         }
     }
 
