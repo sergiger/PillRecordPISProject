bc207eaa1525dbc938794091b856ef8a13c1e42e
joanorteu99 <Joanorteu99@gmail.com>
2020-03-22 14:48:13 +0100

    team funcionalitat acabada

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/team/agenda_team.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/team/agenda_team.kt
@@ -2,8 +2,11 @@ package com.example.afontgou17alumnes.mypillrecord.ui.team
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ImageButton
 import com.example.afontgou17alumnes.mypillrecord.R
+import kotlinx.android.synthetic.main.agenda_team_activity.*
 
 class agenda_team : AppCompatActivity() {
 
@@ -16,5 +19,31 @@ class agenda_team : AppCompatActivity() {
             onBackPressed()
         }
 
+        val persones: ArrayList<String> = ArrayList()
+        addPersones(persones)
+
+        rv_contactes_list.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+        rv_contactes_list.layoutManager = GridLayoutManager(this, 1)
+
+        // Access the RecyclerView Adapter and load the data into it
+        rv_contactes_list.adapter = personAdapter(persones, this)
+
+    }
+
+    fun addPersones(persones:ArrayList<String> = ArrayList()){
+        persones.add("Alex")
+        persones.add("Sergi")
+        persones.add("Joan")
+        persones.add("Arthur")
+        persones.add("pep")
+        persones.add("victor")
+        persones.add("manel")
+        persones.add("alsa")
+        persones.add("willy")
+        persones.add("gumersindo")
+        persones.add("vichy")
+        persones.add("natalia")
     }
 }
---
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/team/personAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.afontgou17alumnes.mypillrecord.ui.team
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.afontgou17alumnes.mypillrecord.R
+import kotlinx.android.synthetic.main.agenda_person_item_list.view.*
+
+class personAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    // Gets the number of animals in the list
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    // Inflates the item views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.agenda_person_item_list, parent, false))
+    }
+
+    // Binds each animal in the ArrayList to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        holder?.tvPersonType?.text = items.get(position)
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    // Holds the TextView that will add each animal to
+    val tvPersonType = view.tv_person_type
+}
