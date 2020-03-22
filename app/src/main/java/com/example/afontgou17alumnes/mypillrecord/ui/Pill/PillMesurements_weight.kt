478de786ebc1f9fbaece69882ce6477bf2beb3f4
Sergi Ger <sergiger00@gmail.com>
2020-03-22 19:12:32 +0100

    statring with Mesurements

---
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/Pill/PillMesurements_weight.kt
@@ -0,0 +1,13 @@
+package com.example.afontgou17alumnes.mypillrecord.ui.Pill
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.afontgou17alumnes.mypillrecord.R
+
+class PillMesurements_weight : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pill_mesurements_weight)
+    }
+}
