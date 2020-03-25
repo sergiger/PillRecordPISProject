2d7e8f883e6962e05795670ce96d11171e29342b
joanorteu99 <Joanorteu99@gmail.com>
2020-03-26 00:20:37 +0100

    Recolliment del init i end dels datePickers de la part de Pill
    Endreçar codi d'aquestos

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/Pill/PillMesurements_Arterialpressure.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/Pill/PillMesurements_Arterialpressure.kt
@@ -5,51 +5,35 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
+import android.widget.DatePicker
 import android.widget.ImageButton
 import android.widget.Toast
 import com.example.afontgou17alumnes.mypillrecord.MainActivity
 import com.example.afontgou17alumnes.mypillrecord.R
 import kotlinx.android.synthetic.main.activity_pill_sports.*
 import kotlinx.android.synthetic.main.specific_dates_dialoge.view.*
+import kotlinx.android.synthetic.main.specific_dates_dialoge.view.cancel
+import kotlinx.android.synthetic.main.weight_dialoge.view.*
+import java.util.*
 
 class PillMesurements_Arterialpressure : AppCompatActivity() {
+
+    var ini_day=0
+    var ini_month=0
+    var ini_year=0
+    var end_day=0
+    var end_month=0
+    var end_year=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pill_mesurements__arterialpressure)
         val image_view = findViewById(R.id.left_arrow) as ImageButton
         image_view.setOnClickListener {
-            onBackPressed()
+            go_back()
         }
         btn_from.setOnClickListener {
-            //dialoge_date()
-            val mDialogView =
-                LayoutInflater.from(this).inflate(R.layout.specific_dates_dialoge, null)
-            //AlertDialogBuilder
-            val mBuilder = AlertDialog.Builder(this)
-                .setView(mDialogView)
-                .setTitle("Set specific dates")
-            //show dialog
-            val mAlertDialog = mBuilder.show()
-            mDialogView.OK.setOnClickListener {
-                /*val datePicker_ini = findViewById<DatePicker>(R.id.date_Picker_ini)
-                val today = Calendar.getInstance()
-                datePicker_ini.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
-                    today.get(Calendar.DAY_OF_MONTH)
-
-                ) { view, year, month, day ->
-                    val month = month + 1
-                    val msg = "You Selected: $day/$month/$year"
-                    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-                }*/
-                Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
-                //TODO AGAFAR DÂDES DELS DIES
-                mAlertDialog.dismiss()
-            }
-            mDialogView.cancel.setOnClickListener {
-                mAlertDialog.dismiss()
-                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show()
-            }
+            select_start_end_dates()
         }
         btn_frequency.setOnClickListener {
             val intent = Intent(this, PillFrequency::class.java)
@@ -57,8 +41,69 @@ class PillMesurements_Arterialpressure : AppCompatActivity() {
         }
         btn_Save.setOnClickListener {
             Toast.makeText(this, "New plan added", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            save_mesurement()
+            go_home()
         }
     }
+
+    fun select_start_end_dates(){
+
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.specific_dates_dialoge, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Set specific dates")
+        //show dialog
+        val  mAlertDialog = mBuilder.show()
+        //Aqui guardo la data INI seleccionada
+        //i també poso la data correcta al DatePicker de INICI amb la data d'avui
+        val datePicker_ini = mDialogView.findViewById<DatePicker>(R.id.date_Picker_ini)
+        val today = Calendar.getInstance()
+        datePicker_ini.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, month, day ->
+            val month = month + 1
+            ini_day=day
+            ini_month=month
+            ini_year=year
+            //val msg = "You Selected: $day/$month/$year"
+            //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+        //Aqui guardo la data END seleccionada
+        //i també poso la data correcta al DatePicker de END amb la data d'avui
+        val datePicker_end = mDialogView.findViewById<DatePicker>(R.id.date_Picker_end)
+        datePicker_end.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, month, day ->
+            val month = month + 1
+            end_day=day
+            end_month=month
+            end_year=year
+            //val msg = "You Selected: $day/$month/$year"
+            //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+
+        //Aqui faig els listeners dels dos botons
+        mDialogView.OK.setOnClickListener {
+            Toast.makeText(this,"work in progress",Toast.LENGTH_LONG).show()
+            //Ara ja estan les hores agafades correctament, ara hauriem de compartir
+            //Les terapies actives i no actives entre aquestes dates
+            mAlertDialog.dismiss()
+        }
+        mDialogView.cancel.setOnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(this,"Cancel",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun go_home(){
+        val intent = Intent(this, MainActivity::class.java);
+        startActivity(intent);
+    }
+
+    fun go_back(){
+        onBackPressed()
+    }
+
+    fun save_mesurement(){}//cal completar
 }
