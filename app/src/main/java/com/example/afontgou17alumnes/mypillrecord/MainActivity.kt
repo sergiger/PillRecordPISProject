096551ad17abbdc695ded2961f0b3fd3791ca325
Agfont <arthurfont2000@gmail.com>
2020-03-26 16:50:39 -0300

    Reparos al calendar

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/MainActivity.kt
@@ -20,57 +20,36 @@ import com.example.afontgou17alumnes.mypillrecord.ui.today.Today_Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
-
 class MainActivity : AppCompatActivity() {
-    var contador=0
-    var primer=0
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{
        item->
        when(item.itemId){
            R.id.action_Today->{
-               contador=contador+1
                replaceFragment(Today_Fragment())
                Toast.makeText(this,"Today",Toast.LENGTH_SHORT).show()
                toolbar.title = "TODAY"
                return@OnNavigationItemSelectedListener true
            }
            R.id.action_calendar->{
-
                replaceFragment(Calendar_fragment())
-               //FragmentManager.popBackStack("calendar",)
-
                Toast.makeText(this,"Calendar",Toast.LENGTH_SHORT).show()
                toolbar.title ="CALENDAR"
-               /*if(primer==0){
-                   replaceFragment(Calendar_fragment())
-                   primer=1
-                   contador=0
-               }else{
-               while(contador>0){
-                   contador= contador-1
-                   getFragmentManager().popBackStack()
-               }}*/
                return@OnNavigationItemSelectedListener true
-               //val new_intent = Intent(this, TempActivity::class.java)
-               //startActivity(new_intent)
            }
            R.id.action_team->{
-               contador=contador+1
                replaceFragment(Team_fragment())
                Toast.makeText(this,"Team",Toast.LENGTH_SHORT).show()
                toolbar.title ="TEAM"
                return@OnNavigationItemSelectedListener true
            }
            R.id.action_pills->{
-               contador=contador+1
                replaceFragment(Pill_fragment())
                Toast.makeText(this,"Add",Toast.LENGTH_SHORT).show()
                toolbar.title ="THERAPY"
                return@OnNavigationItemSelectedListener true
            }
            R.id.action_statistcs-> {
-               contador=contador+1
                replaceFragment(Statistics_fragment())
                Toast.makeText(this,"Statistics",Toast.LENGTH_SHORT).show()
                toolbar.title ="STATISTICS"
@@ -89,7 +68,6 @@ class MainActivity : AppCompatActivity() {
         navigationBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         replaceFragment(Today_Fragment())
 
-
     }
     private fun replaceFragment(fragment: Fragment){
 
@@ -103,12 +81,12 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         val id = item?.itemId
-        if(id==R.id.options_item){
+        if(id == R.id.options_item){
             Toast.makeText(this,"opcions",Toast.LENGTH_SHORT).show()
             val intent = Intent(this, ajustes_activity::class.java);
             startActivity(intent);
         }
-        if(id== R.id.pdf_item){
+        if(id == R.id.pdf_item){
             Toast.makeText(this,"PDF is being generated",Toast.LENGTH_LONG).show()
         }
         return super.onOptionsItemSelected(item)
--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/calendar/Calendar_fragment.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/calendar/Calendar_fragment.kt
@@ -11,29 +11,29 @@ import android.view.ViewGroup
 
 import com.example.afontgou17alumnes.mypillrecord.R
 
-
 class Calendar_fragment : Fragment() {
 
     companion object {
         fun newInstance() = Calendar_fragment()
     }
 
+    private lateinit var viewModel: CalendarFragmentViewModel
     var tabLayout: TabLayout? = null
     var viewPager: ViewPager? = null
-    private lateinit var viewModel: CalendarFragmentViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.calendar_fragment_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        createMonthCalendar()
+    }
 
-        tabLayout = view!!.findViewById<TabLayout>(R.id.tabLayout) //
-        viewPager = view!!.findViewById<ViewPager>(R.id.viewPager) //
+    fun createMonthCalendar() {
+        tabLayout = view!!.findViewById<TabLayout>(R.id.tabLayout)
+        viewPager = view!!.findViewById<ViewPager>(R.id.viewPager)
 
         tabLayout!!.addTab(tabLayout!!.newTab().setText("Week"))
         tabLayout!!.addTab(tabLayout!!.newTab().setText("Month"))
@@ -48,16 +48,12 @@ class Calendar_fragment : Fragment() {
             override fun onTabReselected(tab: TabLayout.Tab) {
                 viewPager!!.currentItem = tab.position
             }
-
-            override fun onTabUnselected(p0: TabLayout.Tab) {
-            }
-
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabSelected(tab: TabLayout.Tab) {
                 viewPager!!.currentItem = tab.position
             }
         })
-        viewModel = ViewModelProviders.of(this).get(CalendarFragmentViewModel::class.java)
+        // viewModel = ViewModelProviders.of(this).get(CalendarFragmentViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
