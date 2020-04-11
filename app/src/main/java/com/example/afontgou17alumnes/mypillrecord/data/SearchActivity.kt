89518f20ae11dd3c8673294bb331dec2eb83db18
Agfont <arthurfont2000@gmail.com>
2020-04-11 12:12:40 -0300

    Search medicines

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/data/SearchActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/data/SearchActivity.kt
@@ -1,7 +1,5 @@
 package com.example.afontgou17alumnes.mypillrecord.data
 
-import android.app.SearchManager
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
@@ -9,15 +7,19 @@ import com.example.afontgou17alumnes.mypillrecord.R
 import com.example.afontgou17alumnes.mypillrecord.R.layout.activity_search
 
 class SearchActivity : AppCompatActivity() {
+    //lateinit var drugs_list: RecyclerView
+    //var drugs: MutableList<String> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_search)
-
-        if (Intent.ACTION_SEARCH == intent.action) {
+        doMySearch()
+        // No entra aqui
+        /*if (Intent.ACTION_SEARCH == intent.action) {
+            println(1)
             intent.getStringExtra(SearchManager.QUERY)?.also { query->
                 doMySearch()
             }
-        }
+        }*/
     }
 
     fun doMySearch() {
@@ -32,6 +34,7 @@ class SearchActivity : AppCompatActivity() {
                 //TODO("Not yet implemented")
                 if (newText!!.isNotEmpty()) {
                     val search = newText.toLowerCase()
+                    println(1111111111)
                     return true
                 }else {
                     return false
