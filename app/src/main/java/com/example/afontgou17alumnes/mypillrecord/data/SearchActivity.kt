3483fc8dd23ed9527d4d527ba7e02a884698ccf0
Agfont <arthurfont2000@gmail.com>
2020-04-12 00:41:26 -0300

    Search medicines

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/data/SearchActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/data/SearchActivity.kt
@@ -1,45 +1,98 @@
 package com.example.afontgou17alumnes.mypillrecord.data
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.afontgou17alumnes.mypillrecord.R
+import com.example.afontgou17alumnes.mypillrecord.R.id.search_src_text
 import com.example.afontgou17alumnes.mypillrecord.R.layout.activity_search
+import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.pill_child.view.*
 
 class SearchActivity : AppCompatActivity() {
-    //lateinit var drugs_list: RecyclerView
-    //var drugs: MutableList<String> = ArrayList()
+    var drugs: MutableList<String> = ArrayList()
+    var displayList: MutableList<String> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_search)
-        doMySearch()
-        // No entra aqui
-        /*if (Intent.ACTION_SEARCH == intent.action) {
-            println(1)
-            intent.getStringExtra(SearchManager.QUERY)?.also { query->
-                doMySearch()
-            }
-        }*/
+        pills_list.layoutManager = LinearLayoutManager(this)
+        pills_list.adapter = PillAdapter(displayList, this)
+        // doMySearch()
+        loadData()
     }
 
-    fun doMySearch() {
-        val finder:SearchView = findViewById(R.id.searchEngine)
-        finder.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                //TODO("Not yet implemented")
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                //TODO("Not yet implemented")
-                if (newText!!.isNotEmpty()) {
-                    val search = newText.toLowerCase()
-                    println(1111111111)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        val searchItem = menu.findItem(R.id.options_search)
+        if (searchItem != null) {
+            val finder = searchItem.actionView as SearchView
+            //val finder:SearchView = findViewById(R.id.searchEngine)
+            val editext:EditText = finder.findViewById(search_src_text)
+            editext.hint = "Enter a medicine here..."
+            finder.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
                     return true
-                }else {
-                    return false
                 }
-            }
-        })
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    displayList.clear()
+                    if (newText!!.isNotEmpty()) {
+                        val search = newText.toLowerCase()
+                        println(search)
+                        drugs.forEach {
+                            if (it.toLowerCase().contains(search)) {
+                                displayList.add(it)
+                            }
+                        }
+                    }else {
+                        displayList.addAll(drugs)
+                    }
+                    pills_list.adapter?.notifyDataSetChanged()
+                    return true
+                }
+            })
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun loadData() {
+        drugs.add("Advil")
+        drugs.add("Bayer")
+        drugs.add("Ibuprofeno")
+        drugs.add("Nasa")
+        drugs.add("Cabrito")
+    }
+
+    /*fun doMySearch() {
+    }*/
+
+    class PillAdapter(items : List<String>, ctx : Context) : RecyclerView.Adapter<PillAdapter.ViewHolder>(){
+        private var list = items
+        private var context = ctx
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.pill_child, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.name?.text = list[position]
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        class ViewHolder (v : View) : RecyclerView.ViewHolder(v) {
+            val name = v.pill_name
+        }
+
     }
 }
