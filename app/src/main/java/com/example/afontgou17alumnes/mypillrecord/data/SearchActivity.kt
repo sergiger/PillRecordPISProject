211affad3ead0e516576dd55d88c5a454ae93dea
Agfont <arthurfont2000@gmail.com>
2020-04-13 03:21:21 -0300

    #15:THERAPY: Search engine of pills

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/data/SearchActivity.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/data/SearchActivity.kt
@@ -3,7 +3,6 @@ package com.example.afontgou17alumnes.mypillrecord.data
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
@@ -23,56 +22,57 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_search)
+
+        back_arrow_search.setOnClickListener{
+            onBackPressed()
+        }
         pills_list.layoutManager = LinearLayoutManager(this)
         pills_list.adapter = PillAdapter(displayList, this)
-        // doMySearch()
         loadData()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu,menu)
-        val searchItem = menu.findItem(R.id.options_search)
-        if (searchItem != null) {
-            val finder = searchItem.actionView as SearchView
-            //val finder:SearchView = findViewById(R.id.searchEngine)
-            val editext:EditText = finder.findViewById(search_src_text)
-            editext.hint = "Enter a medicine here..."
-            finder.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    return true
-                }
-
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    displayList.clear()
-                    if (newText!!.isNotEmpty()) {
-                        val search = newText.toLowerCase()
-                        println(search)
-                        drugs.forEach {
-                            if (it.toLowerCase().contains(search)) {
-                                displayList.add(it)
-                            }
-                        }
-                    }else {
-                        displayList.addAll(drugs)
-                    }
-                    pills_list.adapter?.notifyDataSetChanged()
-                    return true
-                }
-            })
-        }
-        return super.onCreateOptionsMenu(menu)
+        doMySearch()
     }
 
     fun loadData() {
         drugs.add("Advil")
+        drugs.add("Advil")
+        drugs.add("Advil1")
+        drugs.add("Advil2")
+        drugs.add("Advil3")
+        drugs.add("Advil4")
         drugs.add("Bayer")
         drugs.add("Ibuprofeno")
         drugs.add("Nasa")
         drugs.add("Cabrito")
     }
 
-    /*fun doMySearch() {
-    }*/
+    fun doMySearch() {
+        //val finder = searchItem.actionView as SearchView
+        val finder:SearchView = findViewById(R.id.searchEngine)
+        val editext:EditText = finder.findViewById(search_src_text)
+        editext.hint = "Enter medicine name..."
+        finder.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                displayList.clear()
+                if (newText!!.isNotEmpty()) {
+                    val search = newText.toLowerCase()
+                    println(search)
+                    drugs.forEach {
+                        if (it.toLowerCase().contains(search)) {
+                            displayList.add(it)
+                        }
+                    }
+                }else {
+                    displayList.addAll(drugs)
+                }
+                pills_list.adapter?.notifyDataSetChanged()
+                return true
+            }
+        })
+    }
 
     class PillAdapter(items : List<String>, ctx : Context) : RecyclerView.Adapter<PillAdapter.ViewHolder>(){
         private var list = items
