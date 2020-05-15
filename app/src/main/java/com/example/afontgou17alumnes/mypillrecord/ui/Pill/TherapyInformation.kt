826d0d172fbba5e03a31607485c2512876b59cc8
alendinez <55415649+alendinez@users.noreply.github.com>
2020-05-15 15:33:47 +0200

    Canvis fets a layout therapies

--- app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/Pill/TherapyInformation.kt
+++ app/src/main/java/com/example/afontgou17alumnes/mypillrecord/ui/Pill/TherapyInformation.kt
@@ -1,18 +1,26 @@
 package com.example.afontgou17alumnes.mypillrecord.ui.Pill
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
-import android.widget.PopupMenu
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.afontgou17alumnes.mypillrecord.R
 import com.example.afontgou17alumnes.mypillrecord.data.model.therapy.ActivityTherapy
 import com.example.afontgou17alumnes.mypillrecord.data.model.therapy.MeasurementTherapy
 import com.example.afontgou17alumnes.mypillrecord.data.model.therapy.MedicineTherapy
 import com.example.afontgou17alumnes.mypillrecord.data.model.therapy.Therapy
+import kotlinx.android.synthetic.main.number_dialog.view.*
+import kotlinx.android.synthetic.main.specific_dates_dialoge.view.*
 import kotlinx.android.synthetic.main.therapy_information.*
+import kotlinx.android.synthetic.main.time_dialog.view.*
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class TherapyInformation : AppCompatActivity() {
@@ -58,7 +66,10 @@ class TherapyInformation : AppCompatActivity() {
             1->freqDailySelected()
             2->freqEachXDaysSelected()
             3->freqSpecificDaysSelected()
-            4->freqPunctualDaysSelected()
+            4->{
+                freqPunctualDaysSelected()
+                createPunctualDaysList()
+            }
         }
 
         //Listeners
@@ -73,6 +84,26 @@ class TherapyInformation : AppCompatActivity() {
         unit_button_therapy_information.setOnClickListener {
             showPopUpMenuUnits(it)
         }
+
+        add_hour_therapy_information.setOnClickListener {
+            addHour()
+        }
+
+        add_punctual_day.setOnClickListener {
+            addDay()
+        }
+
+        info_button.setOnClickListener {
+            infoButtonModifier()
+        }
+
+        each_days_therapy_information.setOnClickListener {
+            eachXDaysModifier()
+        }
+
+        fromto_layout_therapy_information.setOnClickListener {
+            fromToButton()
+        }
     }
 
     fun createHoursList(){
@@ -82,9 +113,15 @@ class TherapyInformation : AppCompatActivity() {
     }
 
     fun createPunctualDaysList(){
-        val punctualDaysListAdapter = PunctualDaysListAdapter(this, therapy.frequency.listofpuntualdays)
+        val punctualDaysListAdapter = PunctualDaysListAdapter(this, therapy.frequency.listofpuntualdays.toCollection(ArrayList()))
         punctual_days_listview.adapter = punctualDaysListAdapter
         justifyListViewHeightBasedOnChildren(punctual_days_listview)
+    }
+
+    fun refreshDaysList(daysList : ArrayList<String>){
+        val array = Array<String>(daysList.size) {_ -> ""}
+        daysList.toArray(array)
+        therapy.frequency.listofpuntualdays = array
     }
 
     private fun justifyListViewHeightBasedOnChildren(listView: ListView) {
@@ -129,6 +166,7 @@ class TherapyInformation : AppCompatActivity() {
 
     private fun freqDailySelected(){
         frquency_button_therapy_information.text = "Daily"
+        fromto_layout_therapy_information.visibility =  View.VISIBLE
         each_days_therapy_information.visibility = View.GONE
         checkbox_days_layout.visibility = View.GONE
         punctual_days_layout.visibility = View.GONE
@@ -136,6 +174,7 @@ class TherapyInformation : AppCompatActivity() {
 
     private fun freqEachXDaysSelected(){
         frquency_button_therapy_information.text = "Each X days"
+        fromto_layout_therapy_information.visibility =  View.VISIBLE
         each_days_therapy_information.visibility = View.VISIBLE
         if(therapy.frequency.eachtimedose == 0)
             each_days_therapy_information.text = "Each X day(s)"
@@ -148,15 +187,247 @@ class TherapyInformation : AppCompatActivity() {
 
     private fun freqSpecificDaysSelected(){
         frquency_button_therapy_information.text = "Specific week days"
+        fromto_layout_therapy_information.visibility =  View.VISIBLE
         each_days_therapy_information.visibility = View.GONE
         checkbox_days_layout.visibility = View.VISIBLE
+        checkDays()
         punctual_days_layout.visibility = View.GONE
     }
 
     private fun freqPunctualDaysSelected(){
         frquency_button_therapy_information.text =  "Punctual days"
+        fromto_layout_therapy_information.visibility =  View.GONE
         each_days_therapy_information.visibility = View.GONE
         checkbox_days_layout.visibility = View.GONE
         punctual_days_layout.visibility = View.VISIBLE
+    }
+
+    private fun addHour(){
+        var newHour= Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        var newMinute= Calendar.getInstance().get(Calendar.MINUTE)
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.time_dialog, null)
+
+        val ok = mDialogView.findViewById<Button>(R.id.OK)
+        val cancel = mDialogView.findViewById<Button>(R.id.cancel)
+
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Set time")
+        val mAlertDialog = mBuilder.show()
+        mDialogView.time_Picker.setOnTimeChangedListener { view, hour, minute ->
+            newHour = hour
+            newMinute = minute
+        }
+        ok.setOnClickListener {
+            therapy.hours.add("$newHour:$newMinute")
+            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+            mAlertDialog.dismiss()
+            createHoursList()
+        }
+        cancel.setOnClickListener {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            mAlertDialog.dismiss()
+        }
+
+    }
+
+    private fun addDay(){
+        var new_day=Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        var new_month=Calendar.getInstance().get(Calendar.MONTH)+1
+        var new_year=Calendar.getInstance().get(Calendar.YEAR)
+
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.date_dialoge, null)
+
+        val ok = mDialogView.findViewById<Button>(R.id.OK)
+        val cancel = mDialogView.findViewById<Button>(R.id.cancel)
+
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Set specific dates")
+        //show dialog
+        val  mAlertDialog = mBuilder.show()
+        val datePicker_ini = mDialogView.findViewById<DatePicker>(R.id.date_Picker)
+        val today = Calendar.getInstance()
+        datePicker_ini.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, month, day ->
+            val month = month + 1
+            new_day=day
+            new_month=month
+            new_year=year
+        }
+        //Aqui faig els listeners dels dos botons
+        ok.setOnClickListener {
+            Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show()
+            val array = therapy.frequency.listofpuntualdays.toCollection(ArrayList())
+            array.add("$new_day/$new_month/$new_year")
+            refreshDaysList(array)
+            createPunctualDaysList()
+            mAlertDialog.dismiss()
+        }
+        cancel.setOnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(this,"Cancel",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun infoButtonModifier(){
+        var infoValue = 0
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.number_dialog, null)
+        //Set Number Picker
+        mDialogView.number_Picker.minValue = 1
+        mDialogView.number_Picker.maxValue = 100
+        mDialogView.number_Picker.wrapSelectorWheel = false
+
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+
+        //Set title of dialog
+        if(therapy is MedicineTherapy)  mBuilder.setTitle("Set dose")
+        else mBuilder.setTitle("Set duration")
+
+        val mAlertDialog = mBuilder.show()
+        val ok = mDialogView.findViewById<Button>(R.id.OK)
+        val cancel = mDialogView.findViewById<Button>(R.id.cancel)
+
+        mDialogView.number_Picker.setOnValueChangedListener { picker, oldVal, newVal ->
+            infoValue = newVal
+        }
+        ok.setOnClickListener {
+            if(therapy is MedicineTherapy){
+                (therapy as MedicineTherapy).dose = infoValue
+                info_button.text = (therapy as MedicineTherapy).dose.toString()
+            }
+            else if(therapy is ActivityTherapy){
+                (therapy as ActivityTherapy).duration = infoValue
+                info_button.text = (therapy as ActivityTherapy).duration.toString() + " min"
+            }
+            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+            mAlertDialog.dismiss()
+        }
+        cancel.setOnClickListener {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            mAlertDialog.dismiss()
+        }
+    }
+
+    private fun eachXDaysModifier(){
+        var infoValue = 0
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.number_dialog, null)
+        //Set Number Picker
+        mDialogView.number_Picker.minValue = 1
+        mDialogView.number_Picker.maxValue = 100
+        mDialogView.number_Picker.wrapSelectorWheel = false
+
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+
+        //Set title of dialog
+        if(therapy is MedicineTherapy)  mBuilder.setTitle("Set dose")
+        else mBuilder.setTitle("Set duration")
+
+        val mAlertDialog = mBuilder.show()
+        val ok = mDialogView.findViewById<Button>(R.id.OK)
+        val cancel = mDialogView.findViewById<Button>(R.id.cancel)
+
+        mDialogView.number_Picker.setOnValueChangedListener { picker, oldVal, newVal ->
+            infoValue = newVal
+        }
+        ok.setOnClickListener {
+            therapy.frequency.eachtimedose = infoValue
+            each_days_therapy_information.text = "Each $infoValue day(s)"
+            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+            mAlertDialog.dismiss()
+        }
+        cancel.setOnClickListener {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            mAlertDialog.dismiss()
+        }
+    }
+
+    fun checkDays(){
+        val boxes = arrayOf(checkBox_monday, checkBox_tuesday, checkBox_wednesday, checkBox_thursday,
+            checkBox_friday, checkBox_saturday, checkBox_sunday)
+        for( i in therapy.frequency.specificweekdays.indices){
+            if(therapy.frequency.specificweekdays[i] != "")
+                boxes[i].isChecked = true
+        }
+    }
+
+    fun fromToButton(){
+        var ini_day=Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        var ini_month=Calendar.getInstance().get(Calendar.MONTH)+1
+        var ini_year=Calendar.getInstance().get(Calendar.YEAR)
+        var end_day=Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        var end_month=Calendar.getInstance().get(Calendar.MONTH)+1
+        var end_year=Calendar.getInstance().get(Calendar.YEAR)
+
+
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.specific_dates_dialoge, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Set specific dates")
+        //show dialog
+        val  mAlertDialog = mBuilder.show()
+        val ok = mDialogView.findViewById<Button>(R.id.OK)
+        val cancel = mDialogView.findViewById<Button>(R.id.cancel)
+
+        val datePicker_ini = mDialogView.findViewById<DatePicker>(R.id.date_Picker_ini)
+        val today = Calendar.getInstance()
+        datePicker_ini.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, month, day ->
+            val month = month + 1
+            ini_day=day
+            ini_month=month
+            ini_year=year
+        }
+        val datePicker_end = mDialogView.findViewById<DatePicker>(R.id.date_Picker_end)
+        datePicker_end.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, month, day ->
+            val month = month + 1
+            end_day=day
+            end_month=month
+            end_year=year
+        }
+
+        //Aqui faig els listeners dels dos botons
+        ok.setOnClickListener {
+            val i = set_ok_date(ini_day,ini_month,ini_year,end_day,end_month,end_year)
+            if(i == 0){
+                mAlertDialog.dismiss()
+            }
+            else{
+                Toast.makeText(applicationContext,"Invalid date", Toast.LENGTH_SHORT).show()
+            }
+        }
+        cancel.setOnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(this,"Cancel",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun set_ok_date(ini_day:Int,ini_month:Int,ini_year:Int,end_day:Int,end_month:Int,end_year:Int): Int {
+
+        var data_ini=ini_day.toString()+"/"+ini_month.toString()+"/"+ini_year.toString()
+
+
+        var data_end=end_day.toString()+"/"+end_month.toString()+"/"+end_year.toString()
+        val dateStart = LocalDate.parse(data_ini, DateTimeFormatter.ofPattern("d/M/y"))
+        val dateEnd = LocalDate.parse(data_end, DateTimeFormatter.ofPattern("d/M/y"))
+        if(dateStart<=dateEnd){
+            from_therapy_information.text = data_ini
+            to_therapy_information.text = data_end
+            return 0
+        }
+        else{
+            return 1
+        }
     }
 }
